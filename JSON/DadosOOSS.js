24707a0c99a7d382453ae65cb1fee20bd808e5f9
InfinityGraff <cleto.louco@gmail.com>
2025-07-04 02:19:43 -0300

    remama

---
+++ JSON/DadosOOSS.js
@@ -0,0 +1,33 @@
+const DadosOOSS = [
+    {
+        "Id": 1,
+        "Nome": "OS-009190",
+        "Desc": "",
+        "Link": "1A,2ABC"
+    },
+    {
+        "Id": 2,
+        "Nome": "OS-009213",
+        "Desc": "",
+        "Link": "4A,5AB,6AB"
+    },
+    {
+        "Id": 3,
+        "Nome": "Manual1",
+        "Desc": "",
+        "Link": "7A"
+    },
+    {
+        "Id": 4,
+        "Nome": "OS-009252",
+        "Desc": "",
+        "Link": "21|A,22|AB"
+    },
+    {
+        "Id": 5,
+        "Nome": "OS-009292",
+        "Desc": "",
+        "Link": ""
+    }
+  
+]
--- Lixo.js
+++ Lixo.js
@@ -115,3 +115,7 @@ async function PesquisaKM(inpt){
   RsutFrete.innerHTML = listaLugares[inpt]
   }else{RsutFrete.innerHTML = 'Lugar não Encontrado'}
   }
+
+
+
+
