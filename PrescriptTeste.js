aa4fb6854a4052b66c746f0afd73d70fecd8fa1d
InfinityGraff <138248160+InfinityGraff@users.noreply.github.com>
2026-01-03 13:58:34 -0300

    sabu

--- PrescriptTeste.js
+++ PrescriptTeste.js
@@ -1,20 +1,4 @@
-let VarDropImg = {} // uma Váriavel Global q talvez possa ficar dentro do PreScript Destinada a Receber os Files pra serem usados Depois
-function PrepDrop(Eu,nome,Call=null){
-    Inn(Eu,'<a>Adicionar Imagem</a><input type="file" accept="image/*" class="none">')
-    const f=$('input',Eu),d=$('a',Eu)
-    const Import=F=>{if (!F) return
-        const ext  = F.name.split('.').pop()
-        const Nome = `${nome}.${ext}`
-        Inn(d,`<img Name="${Nome}" src="${URL.createObjectURL(F)}">`)
-        VarDropImg[Nome]=F
-    }
-    Eu.onclick    =()=> f.click(),f.onchange=e=>Import(e.target.files[0]),
-    Eu.ondragover =e=>(Prvn2(e),Add(Eu,'hover')),
-    Eu.ondragleave=_=> Rmv(Eu,'hover'),
-    Eu.ondrop     =e=>(Prvn2(e),Rmv(Eu,'hover'),Import(e.dataTransfer.files[0]))
-}
 
-const Dispinput=e=>e.dispatchEvent(new Event('input', { bubbles: true }))
 function Selecty(Eu){const I = $('input',_td(Eu)) ; Vll(I,Eu.innerText) ; Dispinput(I) ; /*Add_N(Pai(Eu))*/}
 
 // Fazer ele transitar com as SETAS
