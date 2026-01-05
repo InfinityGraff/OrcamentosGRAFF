fd2dc52acc5e19e3d2ee9fbdbf03bba0d1f6d83b
InfinityGraff <138248160+InfinityGraff@users.noreply.github.com>
2026-01-05 02:33:50 -0300

    JJ James

--- BaseGRAFF.js
+++ BaseGRAFF.js
@@ -73,6 +73,76 @@ const SVVG = {
 // 'non'  : 'non_transactional-'
 // 'deb'  : 'debt_notifications-'
 
+const Tmplts = [
+    {
+    "name": "Template 1",
+    "canvas": {
+        "width": 320,
+        "height": 500,
+        "background": "rgb(255, 213, 0)"
+    },
+    "items": [
+        {
+        "id": "2",
+        "type": "Txt",
+        "x": 136,
+        "y": 21,
+        "scale": 2.725,
+        "rotate": 0,
+        "z": 0,
+        "color": "rgb(255, 0, 0)",
+        "font": "IN901XKI"
+        },
+        {
+        "id": "3",
+        "type": "Txt",
+        "x": 141,
+        "y": 57,
+        "scale": 1.7149999999999999,
+        "rotate": 0,
+        "z": 0,
+        "color": "rgb(0, 51, 255)",
+        "font": null
+        },
+        {
+        "id": "4",
+        "type": "Txt",
+        "x": 103,
+        "y": 149,
+        "scale": 1.435,
+        "rotate": 0,
+        "z": 0,
+        "color": null,
+        "font": null
+        },
+        {
+        "id": "5",
+        "type": "Txt",
+        "x": 183,
+        "y": 150,
+        "scale": 1.315,
+        "rotate": 0,
+        "z": 0,
+        "color": null,
+        "font": null
+        },
+        {
+        "id": "6",
+        "type": "Txt",
+        "x": 136,
+        "y": 174,
+        "scale": 1.335,
+        "rotate": 0,
+        "z": 0,
+        "color": null,
+        "font": null
+        }
+    ]
+    }
+
+]
+
+
 const Ramos = {
   'Alimentos e Bebidas': {
       Comr:['Padaria','Depósito de Bebidas','Depósito de Água e Gás','Mercadinho','Frigorifico & Peixe','Frutas & Verduras','Empório','Ração'],
--- BiblGRAFF.js
+++ BiblGRAFF.js
@@ -789,3 +789,49 @@ const SavePDF = async (Div, Nome="download.pdf") => {
     });
 }
 
+
+
+function Selecty(Eu){const I = $('input',_td(Eu)) ; Vll(I,Eu.innerText) ; Dispinput(I) ; /*Add_N(Pai(Eu))*/}
+
+// Fazer ele transitar com as SETAS
+const Tm_Select=(Eu,e,lista)=>{
+    Eu.classList.add('Rltv','MySelect','Rltv','w100')
+    // Atualizar span Baseado na Lista
+    const ExibSpan = `Rmv_N($('span',Pai(this)))`
+    Inn(Eu,`<input class="Stky" placeholder="${D_Brev[e]}" onfocus="${ExibSpan}" oninput="PesqOpt(this,'List')">
+        <span class="Abslt Cl Sugg" style="z-index:10">${Tm_MyOpts(lista,'List')}</span>`
+    )
+}
+function Tm_MyOpts(List,Modo){
+    const Previw = {
+        List :e=>`<p style="pointer-events: none">${e}</p>`,
+        Cors :e=>`<p class="RD" style="background:${GetycCores[e]}">${e}</p>`,
+        Table:e=>``,
+        Card :e=>``}
+    return List.map(e=>`<a class="PT w100 Ct" style="display:flex" onclick="Selecty(this)">${Previw[Modo](e)}</a>`).join('')
+}
+
+const PesqOpt = (Inpt, Modo) => { // Essa função de Pesquisa Pode ser Util em Todos os Tipos de Campos de Pesquisas (Basta eu Passar o Input e o Conteudo a ser Grifado)
+    const Span = $('span', Pai(Inpt)), Val = Inpt.value, Regx = new RegExp(`(${aa(Val)})`, 'gi'),
+          Griff = e => e.textContent.replace(Regx, '<b class="My-Griff" style="background:#8f007c;color:white">$1</b>'),
+          TipoFilt = { List: (e, Val) => aa(e).includes(aa(Val)) };
+    $$('a', Span).forEach(a => {
+        Inn(a, a.textContent);                                    // Remove grifs anteriores
+        if (Val == '') { Rmv_N(a); return }                       // Exibe tudo se campo vazio
+        if (TipoFilt[Modo](a.textContent, Val)) { Rmv_N(a); Inn(a, Griff(a)) } else { Add_N(a) } // Grifado ou oculta
+    });
+    // Preenchimento automatico e auto correção na hora de Digitar
+    // selecionar option automaticamente caso tenha apeans 1
+    // quando pesquiso um nome como "Brilho" em "brilho Local" grifa o brilho e acaba perdendo o espaço
+    // quando vai inserir o valor fica 'BrilhoLocal'
+}
+
+
+// dexia os Selects Cinza caso não tenha nada (Mudar isso pra o INPUT)
+function ValidOpts(){$$('#ORC select').forEach(e=>$$('option',e).length==1 ? Add(e,'sOFF') : Rmv(e,'sOFF'))}
+
+function exibirObjeto() {
+    function adicionaID(arr) {return arr.map((obj, i) => ({ id: i, ...obj }))}
+    const saida = document.getElementById("saida")
+    saida.innerHTML = `<pre>${JSON.stringify(adicionaID(BaseIMG), null, 2)}</pre>`
+}
